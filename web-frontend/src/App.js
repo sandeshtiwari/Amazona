8c643a8697e5018684479f1591294c1d52dae7fa
Sandesh Tiwari <sandeshtiwari@live.com>
2023-08-11 12:07:28 -0500

    feat: Cart screen design and add route to cart screen

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -5,6 +5,7 @@ import { Badge, Container, Nav, Navbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   const { state } = useContext(Store);
@@ -35,6 +36,7 @@ function App() {
           <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
