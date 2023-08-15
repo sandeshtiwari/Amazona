7388957e4ad05ce143f3344b5e36dc27467350bf
stiwari64 <sandeshtiwari@live.com>
2023-08-14 19:54:40 -0500

    feat: Implement payment selection screen

--- web-frontend/src/Store.js
+++ web-frontend/src/Store.js
@@ -10,6 +10,9 @@ const initialState = {
     shippingAddress: localStorage.getItem('shippingAddress')
       ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')
+      : '',
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
@@ -43,15 +46,26 @@ function reducer(state, action) {
       return { ...state, userInfo: action.payload };
     }
     case 'USER_SIGNOUT': {
-      return { ...state, userInfo: null, cart: {
-        cartItems: [],
-        shippingAddress: {}
-      } };
+      return {
+        ...state,
+        userInfo: null,
+        cart: {
+          cartItems: [],
+          shippingAddress: {},
+          paymentMethod: '',
+        },
+      };
     }
     case 'SAVE_SHIPPING_ADDRESS': {
       return {
         ...state,
         cart: { ...state.cart, shippingAddress: action.payload },
+      };
+    }
+    case 'SAVE_PAYMENT_METHOD': {
+      return {
+        ...state,
+        cart: { ...state.cart, paymentMethod: action.payload },
       };
     }
     default:
